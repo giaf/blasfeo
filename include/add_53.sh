cf6136f2e3868265e25b6f208df5b84c2f1322aa
Tommaso Sartor <tmmsartor@gmail.com>
2018-06-12 15:00:02 +0000

    Fix ARM compiler flag

--- include/add_53.sh
+++
@@ -1,2 +0,0 @@
-find ./ -type f -not -path "*git*" -not -path "*add_53*" -exec sed -i 's/defined(TARGET_ARMV8A_ARM_CORTEX_A57)/defined(TARGET_ARMV8A_ARM_CORTEX_A57) || defined(TARGET_ARMV8A_ARM_CORTEX_A53)/g' {} \;
-find ./ -type f -not -path "*git*" -not -path "*add_53*" -exec sed -i 's/defined( TARGET_ARMV8A_ARM_CORTEX_A57 )/defined(TARGET_ARMV8A_ARM_CORTEX_A57) || defined(TARGET_ARMV8A_ARM_CORTEX_A53)/g' {} \;
