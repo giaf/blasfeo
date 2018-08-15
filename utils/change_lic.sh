4d7e67bd85ea7241df1c36e5cf43649c22f87ab7
Tommaso Sartor <tmmsartor@gmail.com>
2018-08-15 13:05:05 +0200

    Add placeholder lics

--- utils/change_lic.sh
+++ utils/change_lic.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
 find .. -name "*.h" -exec bash -c 'for file; do cat new_lic.h >temp && tail -n +28 $file >> temp && cat temp > $file; done;' {} +
+find .. -name "Makefile.*" -exec bash -c 'for file; do cat new_lic.makefile >temp && tail -n +28 $file >> temp && cat temp > $file; done;' {} +
