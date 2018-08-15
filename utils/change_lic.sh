5a6ce40a8707f0f32b943e5447dbbdf0919146eb
giaf <gianfri2008@hotmail.it>
2018-08-15 16:41:43 +0200

    changed lincense in .c filex in auxiliary

--- utils/change_lic.sh
+++ utils/change_lic.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-find . -name "*.S" -exec bash -c 'for file; do cat utils/new_lic_c >temp && tail -n +28 $file >> temp && cat temp > $file; done;' {} +
+#find . -name "*.S" -exec bash -c 'for file; do cat utils/new_lic_c >temp && tail -n +28 $file >> temp && cat temp > $file; done;' {} +
+find ./auxiliary -name "*.c" -exec bash -c 'for file; do cat utils/new_lic_c >temp && tail -n +28 $file >> temp && cat temp > $file; done;' {} +
 #find . -name "*.c" -exec bash -c 'for file; do cat utils/new_lic_c >temp && tail -n +28 $file >> temp && cat temp > $file; done;' {} +
 #find . -name "*.h" -exec bash -c 'for file; do cat utils/new_lic_c >temp && tail -n +28 $file >> temp && cat temp > $file; done;' {} +
 #find . -name "Makefile.*" -exec bash -c 'for file; do cat utils/new_lic.makefile >temp && tail -n +28 $file >> temp && cat temp > $file; done;' {} +
