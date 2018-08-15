c60fed7d7571e588a54a65582ba81ef05469559c
giaf <gianfri2008@hotmail.it>
2018-08-15 16:37:19 +0200

    changed license to .S files

--- utils/change_lic.sh
+++ utils/change_lic.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
-find .. -name "*.h" -exec bash -c 'for file; do cat new_lic.h >temp && tail -n +28 $file >> temp && cat temp > $file; done;' {} +
-find .. -name "Makefile.*" -exec bash -c 'for file; do cat new_lic.makefile >temp && tail -n +28 $file >> temp && cat temp > $file; done;' {} +
+find . -name "*.S" -exec bash -c 'for file; do cat utils/new_lic_c >temp && tail -n +28 $file >> temp && cat temp > $file; done;' {} +
+#find . -name "*.c" -exec bash -c 'for file; do cat utils/new_lic_c >temp && tail -n +28 $file >> temp && cat temp > $file; done;' {} +
+#find . -name "*.h" -exec bash -c 'for file; do cat utils/new_lic_c >temp && tail -n +28 $file >> temp && cat temp > $file; done;' {} +
+#find . -name "Makefile.*" -exec bash -c 'for file; do cat utils/new_lic.makefile >temp && tail -n +28 $file >> temp && cat temp > $file; done;' {} +
