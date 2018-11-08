97936b7e4a72d48dd151d374bb89d9ab8eef887d
giaf <gianfri2008@hotmail.it>
2018-11-08 16:35:05 +0100

    add basic kernel dgemm_nn 8x4 cortexa a53

--- blasfeo_api/d_blas3_lib4.c
+++ blasfeo_api/d_blas3_lib4.c
@@ -857,7 +857,7 @@ loop_00:
 			goto left_12;
 			}
 		}
-#elif defined(TARGET_ARMV8A_ARM_CORTEX_A57)
+#elif defined(TARGET_ARMV8A_ARM_CORTEX_A57) | defined(TARGET_ARMV8A_ARM_CORTEX_A53)
 	for(; i<m-7; i+=8)
 		{
 		j = 0;
@@ -1010,7 +1010,7 @@ loop_CD:
 
 
 
-#if defined(TARGET_X64_INTEL_HASWELL) | defined(TARGET_X64_INTEL_SANDY_BRIDGE) | defined(TARGET_ARMV8A_ARM_CORTEX_A57)
+#if defined(TARGET_X64_INTEL_HASWELL) | defined(TARGET_X64_INTEL_SANDY_BRIDGE) | defined(TARGET_ARMV8A_ARM_CORTEX_A57) | defined(TARGET_ARMV8A_ARM_CORTEX_A53)
 	left_8:
 	j = 0;
 	for(; j<n; j+=4)
