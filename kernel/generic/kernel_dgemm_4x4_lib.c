15784bae5d9ea323f64a1ac1285df299b61d5296
giaf <gianfri2008@hotmail.it>
2018-11-21 21:03:00 +0000

    add kernel_dgemm_nn_4x4_vs_lib4cc cortex a57

--- kernel/generic/kernel_dgemm_4x4_lib.c
+++ kernel/generic/kernel_dgemm_4x4_lib.c
@@ -796,7 +796,7 @@ void kernel_dgemm_nn_4x4_lib4cc(int kmax, double *alpha, double *A, double *B, i
 #endif
 
 
-#if defined(TARGET_GENERIC) || defined(TARGET_X86_AMD_BARCELONA) || defined(TARGET_X86_AMD_JAGUAR) || defined(TARGET_X64_INTEL_CORE) || defined(TARGET_X64_AMD_BULLDOZER) || defined(TARGET_ARMV7A_ARM_CORTEX_A15) || defined(TARGET_ARMV7A_ARM_CORTEX_A7) || defined(TARGET_ARMV8A_ARM_CORTEX_A57) || defined(TARGET_ARMV8A_ARM_CORTEX_A53)
+#if defined(TARGET_GENERIC) || defined(TARGET_X86_AMD_BARCELONA) || defined(TARGET_X86_AMD_JAGUAR) || defined(TARGET_X64_INTEL_CORE) || defined(TARGET_X64_AMD_BULLDOZER) || defined(TARGET_ARMV7A_ARM_CORTEX_A15) || defined(TARGET_ARMV7A_ARM_CORTEX_A7)
 void kernel_dgemm_nn_4x4_vs_lib4cc(int kmax, double *alpha, double *A, double *B, int ldb, double *beta, double *C, int ldc, double *D, int ldd, int m1, int n1)
 	{
 
