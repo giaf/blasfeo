74a2025c84fb2f1526797e064a32596504144340
giaf <gianfri2008@hotmail.it>
2019-03-14 10:43:51 +0100

    add dtrmm_lutn using 4x4 kernels

--- blas_api/dtrmm.c
+++ blas_api/dtrmm.c
@@ -169,8 +169,6 @@ void blasfeo_dtrmm(char *side, char *uplo, char *transa, char *diag, int *pm, in
 				else //if(*diag=='u' | *diag=='U') // _llnu
 					{
 					goto llnu;
-//					printf("\nBLASFEO: dtrmm_llnu: not implemented yet\n");
-//					return;
 					}
 				}
 			else //if(*transa=='t' | *transa=='T' | *transa=='c' | *transa=='C') // _llt
@@ -206,8 +204,9 @@ void blasfeo_dtrmm(char *side, char *uplo, char *transa, char *diag, int *pm, in
 				{
 				if(*diag=='n' | *diag=='N') // _lutn
 					{
-					printf("\nBLASFEO: dtrmm_lutn: not implemented yet\n");
-					return;
+					goto lutn;
+//					printf("\nBLASFEO: dtrmm_lutn: not implemented yet\n");
+//					return;
 					}
 				else //if(*diag=='u' | *diag=='U') // _lutu
 					{
@@ -577,6 +576,76 @@ goto llnu_1_return;
 
 llnu_1_return:
 	free(mem);
+	return;
+
+
+
+/************************************************
+* lutn
+************************************************/
+lutn:
+#if defined(TARGET_X64_INTEL_HASWELL)
+	if(m>=256 | n>=256 | n>K_MAX_STACK)
+#elif defined(TARGET_X64_INTEL_SANDY_BRIDGE)
+	if(m>=64 | n>=64 | n>K_MAX_STACK)
+#else
+	if(m>=12 | n>=12 | n>K_MAX_STACK)
+#endif
+		{
+		pack_tran = 1;
+		goto llnn_1;
+		}
+	else
+		{
+		goto lutn_0;
+		}
+
+lutn_0:
+	pU = pU0;
+	sdu = sdu0;
+
+	ii = 0;
+#if 0//defined(TARGET_X64_INTEL_HASWELL)
+#elif 0//defined(TARGET_X64_INTEL_SANDY_BRIDGE)
+#else
+	for(; ii<n-3; ii+=4)
+		{
+		kernel_dpack_tn_4_lib4(n, B+ii*ldb, ldb, pU);
+		for(jj=0; jj<m-3; jj+=4)
+			{
+			kernel_dtrmm_nn_ru_4x4_tran_lib4c4c(jj, alpha, pU, A+jj*lda, lda, &d_0, pU+jj*ps, B+jj+ii*ldb, ldb);
+			}
+		if(jj<m)
+			{
+			kernel_dtrmm_nn_ru_4x4_tran_vs_lib4c4c(jj, alpha, pU, A+jj*lda, lda, &d_0, pU+jj*ps, B+jj+ii*ldb, ldb, m-jj, n-ii);
+			}
+		}
+	if(ii<n)
+		{
+		goto lutn_0_left_4;
+		}
+#endif
+goto lutn_0_return;
+
+#if defined(TARGET_X64_INTEL_HASWELL)
+lutn_0_left_12:
+goto lutn_0_return;
+#endif
+
+#if defined(TARGET_X64_INTEL_HASWELL) | defined(TARGET_X64_INTEL_SANDY_BRIDGE)
+lutn_0_left_8:
+goto lutn_0_return;
+#endif
+
+lutn_0_left_4:
+	kernel_dpack_tn_4_vs_lib4(n, B+ii*ldb, ldb, pU, n-ii);
+	for(jj=0; jj<m; jj+=4)
+		{
+		kernel_dtrmm_nn_ru_4x4_tran_vs_lib4c4c(jj, alpha, pU, A+jj*lda, lda, &d_0, pU+jj*ps, B+jj+ii*ldb, ldb, m-jj, n-ii);
+		}
+goto lutn_0_return;
+
+lutn_0_return:
 	return;
 
 
