b2e35ced07623450dad16d3ef272e876be471f48
giaf <gianfri2008@hotmail.it>
2021-10-26 11:40:29 +0200

    fix multiple cblas enum definitions

--- include/blasfeo_d_blas_api.h
+++ include/blasfeo_d_blas_api.h
@@ -46,11 +46,14 @@
 
 #ifdef BLAS_API
 #ifdef CBLAS_API
+#ifndef BLASFEO_CBLAS_ENUM
+#define BLASFEO_CBLAS_ENUM
 enum CBLAS_ORDER {CblasRowMajor=101, CblasColMajor=102};
 enum CBLAS_TRANSPOSE {CblasNoTrans=111, CblasTrans=112, CblasConjTrans=113};
 enum CBLAS_UPLO {CblasUpper=121, CblasLower=122};
 enum CBLAS_DIAG {CblasNonUnit=131, CblasUnit=132};
 enum CBLAS_SIDE {CblasLeft=141, CblasRight=142};
+#endif
 #endif
 #endif
 
