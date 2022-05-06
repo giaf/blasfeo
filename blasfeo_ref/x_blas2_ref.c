giaf/blasfeo

commit 2fbf7c447c4a9e33a0869b19099a9af2b55d71b1

all: dgemv & dsymv: not access y when beta==0 + some other fixes

blasfeo_ref/x_blas2_ref.c
@@ -53,6 +53,20 @@ void REF_GEMV_N(int m, int n, REAL alpha, struct XMAT *sA, int ai, int aj, struc
 	REAL *y = sy->pa + yi;
 	REAL *z = sz->pa + zi;
 #if 1 // y reg version
+	if(beta==0.0)
+		{
+		for(ii=0; ii<m; ii++)
+			{
+			z[ii] = 0.0;
+			}
+		}
+	else
+		{
+		for(ii=0; ii<m; ii++)
+			{
+			z[ii] = beta * y[ii];
+			}
+		}
 	ii = 0;
 	for(; ii<m-1; ii+=2)
 		{
@@ -69,8 +83,8 @@ void REF_GEMV_N(int m, int n, REAL alpha, struct XMAT *sA, int ai, int aj, struc
 			y_0 += XMATEL_A(aai+ii+0, aaj+jj) * x[jj];
 			y_1 += XMATEL_A(aai+ii+1, aaj+jj) * x[jj];
 			}
-		z[ii+0] = beta * y[ii+0] + alpha * y_0;
-		z[ii+1] = beta * y[ii+1] + alpha * y_1;
+		z[ii+0] += alpha * y_0;
+		z[ii+1] += alpha * y_1;
 		}
 	for(; ii<m; ii++)
 		{
@@ -79,12 +93,22 @@ void REF_GEMV_N(int m, int n, REAL alpha, struct XMAT *sA, int ai, int aj, struc
 			{
 			y_0 += XMATEL_A(aai+ii, aaj+jj) * x[jj];
 			}
-		z[ii] = beta * y[ii] + alpha * y_0;
+		z[ii] += alpha * y_0;
 		}
 #else // x reg version
-	for(ii=0; ii<n; ii++)
+	if(beta==0.0)
 		{
-		z[ii] = beta * y[ii];
+		for(ii=0; ii<m; ii++)
+			{
+			z[ii] = 0.0;
+			}
+		}
+	else
+		{
+		for(ii=0; ii<m; ii++)
+			{
+			z[ii] = beta * y[ii];
+			}
 		}
 	jj = 0;
 	for(; jj<n-1; jj+=2)
@@ -134,6 +158,20 @@ void REF_GEMV_T(int m, int n, REAL alpha, struct XMAT *sA, int ai, int aj, struc
 	REAL *x = sx->pa + xi;
 	REAL *y = sy->pa + yi;
 	REAL *z = sz->pa + zi;
+	if(beta==0.0)
+		{
+		for(ii=0; ii<n; ii++)
+			{
+			z[ii] = 0.0;
+			}
+		}
+	else
+		{
+		for(ii=0; ii<n; ii++)
+			{
+			z[ii] = beta * y[ii];
+			}
+		}
 	jj = 0;
 	for(; jj<n-1; jj+=2)
 		{
@@ -150,8 +188,8 @@ void REF_GEMV_T(int m, int n, REAL alpha, struct XMAT *sA, int ai, int aj, struc
 			y_0 += XMATEL_A(aai+ii, aaj+(jj+0)) * x[ii];
 			y_1 += XMATEL_A(aai+ii, aaj+(jj+1)) * x[ii];
 			}
-		z[jj+0] = beta * y[jj+0] + alpha * y_0;
-		z[jj+1] = beta * y[jj+1] + alpha * y_1;
+		z[jj+0] += alpha * y_0;
+		z[jj+1] += alpha * y_1;
 		}
 	for(; jj<n; jj++)
 		{
@@ -160,7 +198,7 @@ void REF_GEMV_T(int m, int n, REAL alpha, struct XMAT *sA, int ai, int aj, struc
 			{
 			y_0 += XMATEL_A(aai+ii, aaj+(jj+0)) * x[ii];
 			}
-		z[jj+0] = beta * y[jj+0] + alpha * y_0;
+		z[jj+0] += alpha * y_0;
 		}
 	return;
 	}
@@ -189,9 +227,33 @@ void REF_GEMV_NT(int m, int n, REAL alpha_n, REAL alpha_t, struct XMAT *sA, int 
 	REAL *y_t = sy_t->pa + yi_t;
 	REAL *z_n = sz_n->pa + zi_n;
 	REAL *z_t = sz_t->pa + zi_t;
-	for(ii=0; ii<m; ii++)
+	if(beta_n==0.0)
 		{
-		z_n[ii] = beta_n * y_n[ii];
+		for(ii=0; ii<m; ii++)
+			{
+			z_n[ii] = 0.0;
+			}
+		}
+	else
+		{
+		for(ii=0; ii<m; ii++)
+			{
+			z_n[ii] = beta_n * y_n[ii];
+			}
+		}
+	if(beta_n==0.0)
+		{
+		for(ii=0; ii<n; ii++)
+			{
+			z_t[ii] = 0.0;
+			}
+		}
+	else
+		{
+		for(ii=0; ii<m; ii++)
+			{
+			z_t[ii] = beta_t * y_t[ii];
+			}
 		}
 	for(jj=0; jj<n; jj++)
 		{
@@ -203,7 +265,7 @@ void REF_GEMV_NT(int m, int n, REAL alpha_n, REAL alpha_t, struct XMAT *sA, int 
 			z_n[ii] += a_00 * x_n_0;
 			y_t_0 += a_00 * x_t[ii];
 			}
-		z_t[jj] = beta_t * y_t[jj] + alpha_t * y_t_0;
+		z_t[jj] += alpha_t * y_t_0;
 		}
 	return;
 	}
@@ -226,6 +288,20 @@ void REF_SYMV_L(int m, REAL alpha, struct XMAT *sA, int ai, int aj, struct XVEC 
 	REAL *x = sx->pa + xi;
 	REAL *y = sy->pa + yi;
 	REAL *z = sz->pa + zi;
+	if(beta==0.0)
+		{
+		for(ii=0; ii<m; ii++)
+			{
+			z[ii] = 0.0;
+			}
+		}
+	else
+		{
+		for(ii=0; ii<m; ii++)
+			{
+			z[ii] = beta * y[ii];
+			}
+		}
 	ii = 0;
 	for(; ii<m-1; ii+=2)
 		{
@@ -258,8 +334,8 @@ void REF_SYMV_L(int m, REAL alpha, struct XMAT *sA, int ai, int aj, struct XVEC 
 			y_0 += XMATEL_A(aai+jj, aaj+ii+0) * x[jj];
 			y_1 += XMATEL_A(aai+jj, aaj+ii+1) * x[jj];
 			}
-		z[ii+0] = beta * y[ii+0] + alpha * y_0;
-		z[ii+1] = beta * y[ii+1] + alpha * y_1;
+		z[ii+0] += alpha * y_0;
+		z[ii+1] += alpha * y_1;
 		}
 	for(; ii<m; ii++)
 		{
@@ -273,7 +349,7 @@ void REF_SYMV_L(int m, REAL alpha, struct XMAT *sA, int ai, int aj, struct XVEC 
 			{
 			y_0 += XMATEL_A(aai+jj, aaj+ii) * x[jj];
 			}
-		z[ii] = beta * y[ii] + alpha * y_0;
+		z[ii] += alpha * y_0;
 		}
 	return;
 	}
@@ -296,7 +372,22 @@ void REF_SYMV_L_MN(int m, int n, REAL alpha, struct XMAT *sA, int ai, int aj, st
 	REAL *x = sx->pa + xi;
 	REAL *y = sy->pa + yi;
 	REAL *z = sz->pa + zi;
-	for(ii=0; ii<n; ii++)
+	if(beta==0.0)
+		{
+		for(ii=0; ii<m; ii++)
+			{
+			z[ii] = 0.0;
+			}
+		}
+	else
+		{
+		for(ii=0; ii<m; ii++)
+			{
+			z[ii] = beta * y[ii];
+			}
+		}
+	ii = 0;
+	for(; ii<n; ii++)
 		{
 		y_0 = 0.0;
 		jj = 0;
@@ -308,7 +399,17 @@ void REF_SYMV_L_MN(int m, int n, REAL alpha, struct XMAT *sA, int ai, int aj, st
 			{
 			y_0 += XMATEL_A(aai+jj, aaj+ii) * x[jj];
 			}
-		z[ii] = beta * y[ii] + alpha * y_0;
+		z[ii] += alpha * y_0;
+		}
+	for(; ii<m; ii++)
+		{
+		y_0 = 0.0;
+		jj = 0;
+		for(; jj<n; jj++)
+			{
+			y_0 += XMATEL_A(aai+ii, aaj+jj) * x[jj];
+			}
+		z[ii] += alpha * y_0;
 		}
 	return;
 	}
@@ -331,6 +432,20 @@ void REF_SYMV_U(int m, REAL alpha, struct XMAT *sA, int ai, int aj, struct XVEC 
 	REAL *x = sx->pa + xi;
 	REAL *y = sy->pa + yi;
 	REAL *z = sz->pa + zi;
+	if(beta==0.0)
+		{
+		for(ii=0; ii<m; ii++)
+			{
+			z[ii] = 0.0;
+			}
+		}
+	else
+		{
+		for(ii=0; ii<m; ii++)
+			{
+			z[ii] = beta * y[ii];
+			}
+		}
 	ii = 0;
 	for(; ii<m; ii++)
 		{
@@ -344,7 +459,7 @@ void REF_SYMV_U(int m, REAL alpha, struct XMAT *sA, int ai, int aj, struct XVEC 
 			{
 			y_0 += XMATEL_A(aai+ii, aaj+jj) * x[jj];
 			}
-		z[ii] = beta * y[ii] + alpha * y_0;
+		z[ii] += alpha * y_0;
 		}
 	return;
 	}

blasfeo_hp_cm/dsymv.c
@@ -147,17 +147,35 @@ void blasfeo_hp_dsymv_u(int m, double alpha, struct blasfeo_dmat *sA, int ai, in
 	int ii;
 
 	// copy and scale y into z
-	ii = 0;
-	for(; ii<m-3; ii+=4)
+	if(beta==0.0)
 		{
-		z[ii+0] = beta*y[ii+0];
-		z[ii+1] = beta*y[ii+1];
-		z[ii+2] = beta*y[ii+2];
-		z[ii+3] = beta*y[ii+3];
+		ii = 0;
+		for(; ii<m-3; ii+=4)
+			{
+			z[ii+0] = 0.0;
+			z[ii+1] = 0.0;
+			z[ii+2] = 0.0;
+			z[ii+3] = 0.0;
+			}
+		for(; ii<m; ii++)
+			{
+			z[ii+0] = 0.0;
+			}
 		}
-	for(; ii<m; ii++)
+	else
 		{
-		z[ii+0] = beta*y[ii+0];
+		ii = 0;
+		for(; ii<m-3; ii+=4)
+			{
+			z[ii+0] = beta*y[ii+0];
+			z[ii+1] = beta*y[ii+1];
+			z[ii+2] = beta*y[ii+2];
+			z[ii+3] = beta*y[ii+3];
+			}
+		for(; ii<m; ii++)
+			{
+			z[ii+0] = beta*y[ii+0];
+			}
 		}
 
 	// main loop

blasfeo_hp_pm/d_blas2_lib4.c
@@ -194,17 +194,35 @@ void blasfeo_hp_dgemv_nt(int m, int n, double alpha_n, double alpha_t, struct bl
 	int ii;
 
 	// copy and scale y_n int z_n
-	ii = 0;
-	for(; ii<m-3; ii+=4)
+	if(beta_n==0.0)
 		{
-		z_n[ii+0] = beta_n*y_n[ii+0];
-		z_n[ii+1] = beta_n*y_n[ii+1];
-		z_n[ii+2] = beta_n*y_n[ii+2];
-		z_n[ii+3] = beta_n*y_n[ii+3];
+		ii = 0;
+		for(; ii<m-3; ii+=4)
+			{
+			z_n[ii+0] = 0.0;
+			z_n[ii+1] = 0.0;
+			z_n[ii+2] = 0.0;
+			z_n[ii+3] = 0.0;
+			}
+		for(; ii<m; ii++)
+			{
+			z_n[ii+0] = 0.0;
+			}
 		}
-	for(; ii<m; ii++)
+	else
 		{
-		z_n[ii+0] = beta_n*y_n[ii+0];
+		ii = 0;
+		for(; ii<m-3; ii+=4)
+			{
+			z_n[ii+0] = beta_n*y_n[ii+0];
+			z_n[ii+1] = beta_n*y_n[ii+1];
+			z_n[ii+2] = beta_n*y_n[ii+2];
+			z_n[ii+3] = beta_n*y_n[ii+3];
+			}
+		for(; ii<m; ii++)
+			{
+			z_n[ii+0] = beta_n*y_n[ii+0];
+			}
 		}
 	
 	ii = 0;
@@ -246,17 +264,35 @@ void blasfeo_hp_dsymv_l(int m, double alpha, struct blasfeo_dmat *sA, int ai, in
 	double *z = sz->pa + zi;
 
 	// copy and scale y into z
-	ii = 0;
-	for(; ii<m-3; ii+=4)
+	if(beta==0.0)
 		{
-		z[ii+0] = beta*y[ii+0];
-		z[ii+1] = beta*y[ii+1];
-		z[ii+2] = beta*y[ii+2];
-		z[ii+3] = beta*y[ii+3];
+		ii = 0;
+		for(; ii<m-3; ii+=4)
+			{
+			z[ii+0] = 0.0;
+			z[ii+1] = 0.0;
+			z[ii+2] = 0.0;
+			z[ii+3] = 0.0;
+			}
+		for(; ii<m; ii++)
+			{
+			z[ii+0] = 0.0;
+			}
 		}
-	for(; ii<m; ii++)
+	else
 		{
-		z[ii+0] = beta*y[ii+0];
+		ii = 0;
+		for(; ii<m-3; ii+=4)
+			{
+			z[ii+0] = beta*y[ii+0];
+			z[ii+1] = beta*y[ii+1];
+			z[ii+2] = beta*y[ii+2];
+			z[ii+3] = beta*y[ii+3];
+			}
+		for(; ii<m; ii++)
+			{
+			z[ii+0] = beta*y[ii+0];
+			}
 		}
 
 	// clean up at the beginning
@@ -421,17 +457,35 @@ void blasfeo_hp_dsymv_l_mn(int m, int n, double alpha, struct blasfeo_dmat *sA, 
 	double *z = sz->pa + zi;
 
 	// copy and scale y int z
-	ii = 0;
-	for(; ii<m-3; ii+=4)
+	if(beta==0.0)
 		{
-		z[ii+0] = beta*y[ii+0];
-		z[ii+1] = beta*y[ii+1];
-		z[ii+2] = beta*y[ii+2];
-		z[ii+3] = beta*y[ii+3];
+		ii = 0;
+		for(; ii<m-3; ii+=4)
+			{
+			z[ii+0] = 0.0;
+			z[ii+1] = 0.0;
+			z[ii+2] = 0.0;
+			z[ii+3] = 0.0;
+			}
+		for(; ii<m; ii++)
+			{
+			z[ii+0] = 0.0;
+			}
 		}
-	for(; ii<m; ii++)
+	else
 		{
-		z[ii+0] = beta*y[ii+0];
+		ii = 0;
+		for(; ii<m-3; ii+=4)
+			{
+			z[ii+0] = beta*y[ii+0];
+			z[ii+1] = beta*y[ii+1];
+			z[ii+2] = beta*y[ii+2];
+			z[ii+3] = beta*y[ii+3];
+			}
+		for(; ii<m; ii++)
+			{
+			z[ii+0] = beta*y[ii+0];
+			}
 		}
 
 	// clean up at the beginning

blasfeo_hp_pm/d_blas2_lib8.c
@@ -178,19 +178,37 @@ void blasfeo_hp_dgemv_nt(int m, int n, double alpha_n, double alpha_t, struct bl
 	int ii;
 
 	// copy and scale y_n int z_n
-	ii = 0;
-	for(; ii<m-3; ii+=4)
+	if(beta_n==0.0)
 		{
-		z_n[ii+0] = beta_n*y_n[ii+0];
-		z_n[ii+1] = beta_n*y_n[ii+1];
-		z_n[ii+2] = beta_n*y_n[ii+2];
-		z_n[ii+3] = beta_n*y_n[ii+3];
+		ii = 0;
+		for(; ii<m-3; ii+=4)
+			{
+			z_n[ii+0] = 0.0;
+			z_n[ii+1] = 0.0;
+			z_n[ii+2] = 0.0;
+			z_n[ii+3] = 0.0;
+			}
+		for(; ii<m; ii++)
+			{
+			z_n[ii+0] = 0.0;
+			}
 		}
-	for(; ii<m; ii++)
+	else
 		{
-		z_n[ii+0] = beta_n*y_n[ii+0];
+		ii = 0;
+		for(; ii<m-3; ii+=4)
+			{
+			z_n[ii+0] = beta_n*y_n[ii+0];
+			z_n[ii+1] = beta_n*y_n[ii+1];
+			z_n[ii+2] = beta_n*y_n[ii+2];
+			z_n[ii+3] = beta_n*y_n[ii+3];
+			}
+		for(; ii<m; ii++)
+			{
+			z_n[ii+0] = beta_n*y_n[ii+0];
+			}
 		}
-	
+		
 	ii = 0;
 #if 0
 	for(; ii<n-5; ii+=6)
@@ -232,17 +250,35 @@ void blasfeo_hp_dsymv_l(int m, double alpha, struct blasfeo_dmat *sA, int ai, in
 	double air = ai%ps;
 
 	// copy and scale y int z
-	ii = 0;
-	for(; ii<m-3; ii+=4)
+	if(beta==0.0)
 		{
-		z[ii+0] = beta*y[ii+0];
-		z[ii+1] = beta*y[ii+1];
-		z[ii+2] = beta*y[ii+2];
-		z[ii+3] = beta*y[ii+3];
+		ii = 0;
+		for(; ii<m-3; ii+=4)
+			{
+			z[ii+0] = 0.0;
+			z[ii+1] = 0.0;
+			z[ii+2] = 0.0;
+			z[ii+3] = 0.0;
+			}
+		for(; ii<m; ii++)
+			{
+			z[ii+0] = 0.0;
+			}
 		}
-	for(; ii<m; ii++)
+	else
 		{
-		z[ii+0] = beta*y[ii+0];
+		ii = 0;
+		for(; ii<m-3; ii+=4)
+			{
+			z[ii+0] = beta*y[ii+0];
+			z[ii+1] = beta*y[ii+1];
+			z[ii+2] = beta*y[ii+2];
+			z[ii+3] = beta*y[ii+3];
+			}
+		for(; ii<m; ii++)
+			{
+			z[ii+0] = beta*y[ii+0];
+			}
 		}
 
 	// clean up at the beginning
@@ -295,17 +331,35 @@ void blasfeo_hp_dsymv_l_mn(int m, int n, double alpha, struct blasfeo_dmat *sA, 
 	double air = ai%ps;
 
 	// copy and scale y int z
-	ii = 0;
-	for(; ii<m-3; ii+=4)
+	if(beta==0.0)
 		{
-		z[ii+0] = beta*y[ii+0];
-		z[ii+1] = beta*y[ii+1];
-		z[ii+2] = beta*y[ii+2];
-		z[ii+3] = beta*y[ii+3];
+		ii = 0;
+		for(; ii<m-3; ii+=4)
+			{
+			z[ii+0] = 0.0;
+			z[ii+1] = 0.0;
+			z[ii+2] = 0.0;
+			z[ii+3] = 0.0;
+			}
+		for(; ii<m; ii++)
+			{
+			z[ii+0] = 0.0;
+			}
 		}
-	for(; ii<m; ii++)
+	else
 		{
-		z[ii+0] = beta*y[ii+0];
+		ii = 0;
+		for(; ii<m-3; ii+=4)
+			{
+			z[ii+0] = beta*y[ii+0];
+			z[ii+1] = beta*y[ii+1];
+			z[ii+2] = beta*y[ii+2];
+			z[ii+3] = beta*y[ii+3];
+			}
+		for(; ii<m; ii++)
+			{
+			z[ii+0] = beta*y[ii+0];
+			}
 		}
 
 	// clean up at the beginning

blasfeo_hp_pm/s_blas2_lib4.c
@@ -154,17 +154,35 @@ void blasfeo_hp_sgemv_nt(int m, int n, float alpha_n, float alpha_t, struct blas
 	int ii;
 
 	// copy and scale y_n int z_n
-	ii = 0;
-	for(; ii<m-3; ii+=4)
+	if(beta_n==0.0)
 		{
-		z_n[ii+0] = beta_n*y_n[ii+0];
-		z_n[ii+1] = beta_n*y_n[ii+1];
-		z_n[ii+2] = beta_n*y_n[ii+2];
-		z_n[ii+3] = beta_n*y_n[ii+3];
+		ii = 0;
+		for(; ii<m-3; ii+=4)
+			{
+			z_n[ii+0] = 0.0;
+			z_n[ii+1] = 0.0;
+			z_n[ii+2] = 0.0;
+			z_n[ii+3] = 0.0;
+			}
+		for(; ii<m; ii++)
+			{
+			z_n[ii+0] = 0.0;
+			}
 		}
-	for(; ii<m; ii++)
+	else
 		{
-		z_n[ii+0] = beta_n*y_n[ii+0];
+		ii = 0;
+		for(; ii<m-3; ii+=4)
+			{
+			z_n[ii+0] = beta_n*y_n[ii+0];
+			z_n[ii+1] = beta_n*y_n[ii+1];
+			z_n[ii+2] = beta_n*y_n[ii+2];
+			z_n[ii+3] = beta_n*y_n[ii+3];
+			}
+		for(; ii<m; ii++)
+			{
+			z_n[ii+0] = beta_n*y_n[ii+0];
+			}
 		}
 	
 	ii = 0;
@@ -199,17 +217,35 @@ void blasfeo_hp_ssymv_l(int m, float alpha, struct blasfeo_smat *sA, int ai, int
 	float *z = sz->pa + zi;
 
 	// copy and scale y int z
-	ii = 0;
-	for(; ii<m-3; ii+=4)
+	if(beta==0.0)
 		{
-		z[ii+0] = beta*y[ii+0];
-		z[ii+1] = beta*y[ii+1];
-		z[ii+2] = beta*y[ii+2];
-		z[ii+3] = beta*y[ii+3];
+		ii = 0;
+		for(; ii<m-3; ii+=4)
+			{
+			z[ii+0] = 0.0;
+			z[ii+1] = 0.0;
+			z[ii+2] = 0.0;
+			z[ii+3] = 0.0;
+			}
+		for(; ii<m; ii++)
+			{
+			z[ii+0] = 0.0;
+			}
 		}
-	for(; ii<m; ii++)
+	else
 		{
-		z[ii+0] = beta*y[ii+0];
+		ii = 0;
+		for(; ii<m-3; ii+=4)
+			{
+			z[ii+0] = beta*y[ii+0];
+			z[ii+1] = beta*y[ii+1];
+			z[ii+2] = beta*y[ii+2];
+			z[ii+3] = beta*y[ii+3];
+			}
+		for(; ii<m; ii++)
+			{
+			z[ii+0] = beta*y[ii+0];
+			}
 		}
 	
 	// clean up at the beginning
@@ -260,17 +296,35 @@ void blasfeo_hp_ssymv_l_mn(int m, int n, float alpha, struct blasfeo_smat *sA, i
 	float *z = sz->pa + zi;
 
 	// copy and scale y int z
-	ii = 0;
-	for(; ii<m-3; ii+=4)
+	if(beta==0.0)
 		{
-		z[ii+0] = beta*y[ii+0];
-		z[ii+1] = beta*y[ii+1];
-		z[ii+2] = beta*y[ii+2];
-		z[ii+3] = beta*y[ii+3];
+		ii = 0;
+		for(; ii<m-3; ii+=4)
+			{
+			z[ii+0] = 0.0;
+			z[ii+1] = 0.0;
+			z[ii+2] = 0.0;
+			z[ii+3] = 0.0;
+			}
+		for(; ii<m; ii++)
+			{
+			z[ii+0] = 0.0;
+			}
 		}
-	for(; ii<m; ii++)
+	else
 		{
-		z[ii+0] = beta*y[ii+0];
+		ii = 0;
+		for(; ii<m-3; ii+=4)
+			{
+			z[ii+0] = beta*y[ii+0];
+			z[ii+1] = beta*y[ii+1];
+			z[ii+2] = beta*y[ii+2];
+			z[ii+3] = beta*y[ii+3];
+			}
+		for(; ii<m; ii++)
+			{
+			z[ii+0] = beta*y[ii+0];
+			}
 		}
 	
 	// clean up at the beginning

blasfeo_hp_pm/s_blas2_lib8.c
@@ -944,17 +944,35 @@ void blasfeo_hp_sgemv_nt(int m, int n, float alpha_n, float alpha_t, struct blas
 	int ii;
 
 	// copy and scale y_n int z_n
-	ii = 0;
-	for(; ii<m-3; ii+=4)
+	if(beta_n==0.0)
 		{
-		z_n[ii+0] = beta_n*y_n[ii+0];
-		z_n[ii+1] = beta_n*y_n[ii+1];
-		z_n[ii+2] = beta_n*y_n[ii+2];
-		z_n[ii+3] = beta_n*y_n[ii+3];
+		ii = 0;
+		for(; ii<m-3; ii+=4)
+			{
+			z_n[ii+0] = 0.0;
+			z_n[ii+1] = 0.0;
+			z_n[ii+2] = 0.0;
+			z_n[ii+3] = 0.0;
+			}
+		for(; ii<m; ii++)
+			{
+			z_n[ii+0] = 0.0;
+			}
 		}
-	for(; ii<m; ii++)
+	else
 		{
-		z_n[ii+0] = beta_n*y_n[ii+0];
+		ii = 0;
+		for(; ii<m-3; ii+=4)
+			{
+			z_n[ii+0] = beta_n*y_n[ii+0];
+			z_n[ii+1] = beta_n*y_n[ii+1];
+			z_n[ii+2] = beta_n*y_n[ii+2];
+			z_n[ii+3] = beta_n*y_n[ii+3];
+			}
+		for(; ii<m; ii++)
+			{
+			z_n[ii+0] = beta_n*y_n[ii+0];
+			}
 		}
 	
 	ii = 0;
@@ -989,17 +1007,35 @@ void blasfeo_hp_ssymv_l(int m, float alpha, struct blasfeo_smat *sA, int ai, int
 	float *z = sz->pa + zi;
 
 	// copy and scale y int z
-	ii = 0;
-	for(; ii<m-3; ii+=4)
+	if(beta==0.0)
 		{
-		z[ii+0] = beta*y[ii+0];
-		z[ii+1] = beta*y[ii+1];
-		z[ii+2] = beta*y[ii+2];
-		z[ii+3] = beta*y[ii+3];
+		ii = 0;
+		for(; ii<m-3; ii+=4)
+			{
+			z[ii+0] = 0.0;
+			z[ii+1] = 0.0;
+			z[ii+2] = 0.0;
+			z[ii+3] = 0.0;
+			}
+		for(; ii<m; ii++)
+			{
+			z[ii+0] = 0.0;
+			}
 		}
-	for(; ii<m; ii++)
+	else
 		{
-		z[ii+0] = beta*y[ii+0];
+		ii = 0;
+		for(; ii<m-3; ii+=4)
+			{
+			z[ii+0] = beta*y[ii+0];
+			z[ii+1] = beta*y[ii+1];
+			z[ii+2] = beta*y[ii+2];
+			z[ii+3] = beta*y[ii+3];
+			}
+		for(; ii<m; ii++)
+			{
+			z[ii+0] = beta*y[ii+0];
+			}
 		}
 	
 	// clean up at the beginning
@@ -1050,17 +1086,35 @@ void blasfeo_hp_ssymv_l_mn(int m, int n, float alpha, struct blasfeo_smat *sA, i
 	float *z = sz->pa + zi;
 
 	// copy and scale y int z
-	ii = 0;
-	for(; ii<m-3; ii+=4)
+	if(beta==0.0)
 		{
-		z[ii+0] = beta*y[ii+0];
-		z[ii+1] = beta*y[ii+1];
-		z[ii+2] = beta*y[ii+2];
-		z[ii+3] = beta*y[ii+3];
+		ii = 0;
+		for(; ii<m-3; ii+=4)
+			{
+			z[ii+0] = 0.0;
+			z[ii+1] = 0.0;
+			z[ii+2] = 0.0;
+			z[ii+3] = 0.0;
+			}
+		for(; ii<m; ii++)
+			{
+			z[ii+0] = 0.0;
+			}
 		}
-	for(; ii<m; ii++)
+	else
 		{
-		z[ii+0] = beta*y[ii+0];
+		ii = 0;
+		for(; ii<m-3; ii+=4)
+			{
+			z[ii+0] = beta*y[ii+0];
+			z[ii+1] = beta*y[ii+1];
+			z[ii+2] = beta*y[ii+2];
+			z[ii+3] = beta*y[ii+3];
+			}
+		for(; ii<m; ii++)
+			{
+			z[ii+0] = beta*y[ii+0];
+			}
 		}
 	
 	// clean up at the beginning

blasfeo_ref/d_blas3_ref_blas.c
@@ -105,6 +105,11 @@
 #define REF_TRSM_RUNU blasfeo_hp_cm_dtrsm_runu
 #define REF_TRSM_RUTN blasfeo_hp_cm_dtrsm_rutn
 #define REF_TRSM_RUTU blasfeo_hp_cm_dtrsm_rutu
+// syr2k
+#define REF_SYR2K_LN blasfeo_hp_cm_dsyr2k_ln
+#define REF_SYR2K_LT blasfeo_hp_cm_dsyr2k_lt
+#define REF_SYR2K_UN blasfeo_hp_cm_dsyr2k_un
+#define REF_SYR2K_UT blasfeo_hp_cm_dsyr2k_ut
 
 
 // gemm
@@ -152,6 +157,11 @@
 #define TRSM_RUNU blasfeo_cm_dtrsm_runu
 #define TRSM_RUTN blasfeo_cm_dtrsm_rutn
 #define TRSM_RUTU blasfeo_cm_dtrsm_rutu
+// syr2k
+#define SYR2K_LN blasfeo_cm_dsyr2k_ln
+#define SYR2K_LT blasfeo_cm_dsyr2k_lt
+#define SYR2K_UN blasfeo_cm_dsyr2k_un
+#define SYR2K_UT blasfeo_cm_dsyr2k_ut
 
 
 

blasfeo_ref/s_blas3_ref_blas.c
@@ -105,6 +105,11 @@
 #define REF_TRSM_RUNU blasfeo_hp_cm_strsm_runu
 #define REF_TRSM_RUTN blasfeo_hp_cm_strsm_rutn
 #define REF_TRSM_RUTU blasfeo_hp_cm_strsm_rutu
+// syr2k
+#define REF_SYR2K_LN blasfeo_hp_cm_ssyr2k_ln
+#define REF_SYR2K_LT blasfeo_hp_cm_ssyr2k_lt
+#define REF_SYR2K_UN blasfeo_hp_cm_ssyr2k_un
+#define REF_SYR2K_UT blasfeo_hp_cm_ssyr2k_ut
 
 
 // gemm
@@ -152,6 +157,11 @@
 #define TRSM_RUNU blasfeo_cm_strsm_runu
 #define TRSM_RUTN blasfeo_cm_strsm_rutn
 #define TRSM_RUTU blasfeo_cm_strsm_rutu
+// syr2k
+#define SYR2K_LN blasfeo_cm_ssyr2k_ln
+#define SYR2K_LT blasfeo_cm_ssyr2k_lt
+#define SYR2K_UN blasfeo_cm_ssyr2k_un
+#define SYR2K_UT blasfeo_cm_ssyr2k_ut
 
 
 

commit 686805b7f120b97973cba412c0ea09dffa5e5ebf

fixed bug in REF_GEMV_NT for beta_t!=0, beta_n==0
introduced in 2fbf7c447c4a9e33a0869b19099a9af2b55d71b1

blasfeo_ref/x_blas2_ref.c
@@ -241,7 +241,7 @@ void REF_GEMV_NT(int m, int n, REAL alpha_n, REAL alpha_t, struct XMAT *sA, int 
 			z_n[ii] = beta_n * y_n[ii];
 			}
 		}
-	if(beta_n==0.0)
+	if(beta_t==0.0)
 		{
 		for(ii=0; ii<n; ii++)
 			{